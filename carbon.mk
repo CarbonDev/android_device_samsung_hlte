8a3556c8465241732df696017f15e77bf0f5e0d4
yanniks <kontakt@yanniks.de>
2014-03-03 18:49:58 +0100

    hlte: remove GSM APN list
    Change-Id: Ia5084b7b5822885e94ba0e629d80dc3a405f77b3

--- carbon.mk
+++ carbon.mk
@@ -1,9 +1,6 @@
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
 # Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
-
-# Inherit some common Carbon stuff.
 $(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_DEVICE := hlte
